d86e8f28115f8b188648e4e42c5a653022826414
Marat Pythonist <marat_mityaev@mail.ru>
2023-07-26 23:52:04 +0300

    Ready_Task1_2

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ with open('cook_book.txt', encoding='utf-8') as file:
         for p in range(int(ingredients_count)):
             recepie = file.readline().strip().split(' | ')
             product, quantity, word = recepie
-            ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
+            ingredients.append({'product': product, 'quantity': int(quantity), 'measure': word})
         file.readline()
-    cook_book[recepie_name] = ingredients
+        cook_book[recepie_name] = ingredients
 
-def get_shop_list_by_dishes(person_count, **dishes):
-    for p in ingredients:
-        if p == dishes:
-            quantity = quantity * person_count
-            print(dishes)
+def get_shop_list_by_dishes(person_count, dishes):
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredients in cook_book[dish]:
+                product = ingredients['product']
+                quantity = ingredients['quantity'] * person_count
+                measure = ingredients['measure']
+                print(product, quantity, measure)
 get_shop_list_by_dishes(2, ['Запеченный картофель', 'Омлет'])
+
+
+
 
 
